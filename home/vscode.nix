5c4c33cd61d8e9edd809f792a2be3291193cb29f
Dane Lipscombe <danelipscombe@gmail.com>
2022-12-15 12:42:37 +1100

    Update vscode

--- home/vscode.nix
+++ home/vscode.nix
@@ -1,6 +1,7 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.vscode = {
     enable = true;
+    # There are certain vscode extensions that have native binaries that need to be added here, you can install other plugins normally via vscode ui
     extensions = with pkgs.vscode-extensions; [
       ms-vsliveshare-vsliveshare
     ];
