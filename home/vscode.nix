0348095636157f50b93b7dbfa9af3afcd1628ee1
Dane Lipscombe <danelipscombe@gmail.com>
2022-12-15 12:44:15 +1100

    Fix extension

--- home/vscode.nix
+++ home/vscode.nix
@@ -3,7 +3,7 @@
     enable = true;
     # There are certain vscode extensions that have native binaries that need to be added here, you can install other plugins normally via vscode ui
     extensions = with pkgs.vscode-extensions; [
-      ms-vsliveshare-vsliveshare
+      ms-vsliveshare.vsliveshare
     ];
   };
 }
